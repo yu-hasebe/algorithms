1cf9c1ea1e745fecb7885e6a158cfc77decd3c9f
yu-hasebe <yu.has128@gmail.com>
2020-12-05 18:01:40 +0900

    Add lower_bound

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod carmichael_numbers;
 mod dijkstra;
+mod lower_bound;
 mod sieve_of_eratosthenes;
---
+++ src/lower_bound.rs
@@ -0,0 +1,31 @@
+fn lower_bound(n: usize, a: Vec<i64>, k: i64) -> usize {
+    let mut low = 0;
+    let mut high = a.len();
+    // After the loop, low + 1 == high must be true.
+    while low + 1 < high {
+        let mid = low + (high - low) / 2;
+        if ok(&a, k, mid) {
+            high = mid;
+        // The answer must be in (low, high).
+        } else {
+            low = mid;
+        }
+    }
+    // After the loop, low + 1 == high is true and the answer is in (low, high].
+    // Therefore, the answer is high.
+    high
+}
+
+fn ok(a: &Vec<i64>, k: i64, m: usize) -> bool {
+    k <= a[m]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(1, lower_bound(5, vec![2, 3, 3, 5, 6], 3));
+    }
+}
